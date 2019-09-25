a85e9b284ce871432609d49cdb5ca19d7f888e96
Viorel <Viorel@noreply.github.com>
2019-09-25 15:17:34 +0300

    Adding an option to show whitespaces.

--- RegExpressWPF/Adorners/UnderliningAdorner.cs
+++ RegExpressWPF/Adorners/UnderliningAdorner.cs
@@ -14,135 +14,135 @@ using System.Windows.Threading;
 
 namespace RegExpressWPF.Adorners
 {
-    class UnderliningAdorner : Adorner
-    {
-        readonly Pen Pen = new Pen( Brushes.MediumVioletRed, 2 );
+	class UnderliningAdorner : Adorner
+	{
+		readonly Pen Pen = new Pen( Brushes.MediumVioletRed, 2 );
 
-        IReadOnlyList<(TextPointer start, TextPointer end)> Ranges = null;
-
-
-        public UnderliningAdorner( UIElement adornedElement ) : base( adornedElement )
-        {
-            Debug.Assert( adornedElement is RichTextBox );
-
-            IsHitTestVisible = false;
-
-            Rtb.TextChanged += Rtb_TextChanged;
-            Rtb.AddHandler( ScrollViewer.ScrollChangedEvent, new RoutedEventHandler( Rtb_ScrollChanged ), true );
-        }
+		IReadOnlyList<(TextPointer start, TextPointer end)> Ranges = null;
 
 
-        RichTextBox Rtb
-        {
-            get { return (RichTextBox)AdornedElement; }
-        }
+		public UnderliningAdorner( UIElement adornedElement ) : base( adornedElement )
+		{
+			Debug.Assert( adornedElement is RichTextBox );
+
+			IsHitTestVisible = false;
+
+			Rtb.TextChanged += Rtb_TextChanged;
+			Rtb.AddHandler( ScrollViewer.ScrollChangedEvent, new RoutedEventHandler( Rtb_ScrollChanged ), true );
+		}
 
 
-        private void Rtb_TextChanged( object sender, TextChangedEventArgs e )
-        {
-            Invalidate( );
-        }
-
-        private void Rtb_ScrollChanged( object sender, RoutedEventArgs e )
-        {
-            Invalidate( );
-        }
+		RichTextBox Rtb
+		{
+			get { return (RichTextBox)AdornedElement; }
+		}
 
 
-        protected override void OnRender( DrawingContext drawingContext )
-        {
-            base.OnRender( drawingContext );  // (probably nothing)
+		private void Rtb_TextChanged( object sender, TextChangedEventArgs e )
+		{
+			Invalidate( );
+		}
 
-            var dc = drawingContext;
-            var rtb = Rtb;
-
-            var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
-            dc.PushClip( new RectangleGeometry( clip_rect ) );
-
-            lock( this )
-            {
-                if( Ranges != null )
-                {
-                    var start_doc = rtb.Document.ContentStart;
-
-                    // TODO: clean 'Ranges' if document was changed (release old document)
-
-                    foreach( var (start, end) in Ranges )
-                    {
-                        if( start.HasValidLayout && end.HasValidLayout )
-                        {
-                            if( start.IsInSameDocument( start_doc ) && end.IsInSameDocument( start_doc ) )
-                            {
-                                var start_rect = start.GetCharacterRect( LogicalDirection.Forward );
-                                var end_rect = end.GetCharacterRect( LogicalDirection.Backward );
-
-                                if( !( end_rect.Bottom < clip_rect.Top || start_rect.Top > clip_rect.Bottom ) )
-                                {
-                                    if( start_rect.Bottom > end_rect.Top )
-                                    {
-                                        // no wrap, draw quickly
-
-                                        DrawUnderline( dc, new Rect( start_rect.TopLeft, end_rect.BottomLeft ), isLeftStart: true, isRightEnd: true );
-                                    }
-                                    else
-                                    {
-                                        var adjusted_end = end.GetInsertionPosition( LogicalDirection.Backward );
-                                        Debug.Assert( adjusted_end != null );
-                                        Debug.Assert( adjusted_end.IsAtInsertionPosition );
-
-                                        TextPointer tp = start;
-                                        if( !tp.IsAtInsertionPosition ) tp = tp.GetInsertionPosition( LogicalDirection.Forward );
-                                        Debug.Assert( tp.IsAtInsertionPosition );
-
-                                        RectInfo rect_info = new RectInfo { nextRect = start_rect };
-
-                                        bool is_left_start = true;
-
-                                        for(; ; )
-                                        {
-                                            Rect r = Rect.Empty;
-
-                                            for(; ; )
-                                            {
-                                                rect_info = GetRectInfo( tp, rect_info.nextRect );
-                                                r.Union( rect_info.thisRect );
-
-                                                if( rect_info.nextPointer == null || !IsBefore( rect_info.nextPointer, adjusted_end ) ) { tp = null; break; }
-                                                tp = rect_info.nextPointer;
-                                                if( !rect_info.nextIsSameLine ) { break; }
-                                            }
-
-                                            DrawUnderline( dc, r, is_left_start, tp == null );
-
-                                            if( tp == null ) break;
-
-                                            is_left_start = false;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            dc.Pop( );
-        }
+		private void Rtb_ScrollChanged( object sender, RoutedEventArgs e )
+		{
+			Invalidate( );
+		}
 
 
-        void DrawUnderline( DrawingContext dc, Rect rect, bool isLeftStart, bool isRightEnd )
-        {
-            var guidelines = new GuidelineSet( );
+		protected override void OnRender( DrawingContext drawingContext )
+		{
+			base.OnRender( drawingContext );  // (probably nothing)
 
-            guidelines.GuidelinesX.Add( rect.Left );
-            guidelines.GuidelinesX.Add( rect.Right );
-            guidelines.GuidelinesY.Add( rect.Bottom );
+			var dc = drawingContext;
+			var rtb = Rtb;
 
-            guidelines.Freeze( );
+			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+			dc.PushClip( new RectangleGeometry( clip_rect ) );
 
-            dc.PushGuidelineSet( guidelines );
+			lock( this )
+			{
+				if( Ranges != null )
+				{
+					var start_doc = rtb.Document.ContentStart;
 
-            /*
+					// TODO: clean 'Ranges' if document was changed (release old document)
+
+					foreach( var (start, end) in Ranges )
+					{
+						if( start.HasValidLayout && end.HasValidLayout )
+						{
+							if( start.IsInSameDocument( start_doc ) && end.IsInSameDocument( start_doc ) )
+							{
+								var start_rect = start.GetCharacterRect( LogicalDirection.Forward );
+								var end_rect = end.GetCharacterRect( LogicalDirection.Backward );
+
+								if( !( end_rect.Bottom < clip_rect.Top || start_rect.Top > clip_rect.Bottom ) )
+								{
+									if( start_rect.Bottom > end_rect.Top )
+									{
+										// no wrap, draw quickly
+
+										DrawUnderline( dc, new Rect( start_rect.TopLeft, end_rect.BottomLeft ), isLeftStart: true, isRightEnd: true );
+									}
+									else
+									{
+										var adjusted_end = end.GetInsertionPosition( LogicalDirection.Backward );
+										Debug.Assert( adjusted_end != null );
+										Debug.Assert( adjusted_end.IsAtInsertionPosition );
+
+										TextPointer tp = start;
+										if( !tp.IsAtInsertionPosition ) tp = tp.GetInsertionPosition( LogicalDirection.Forward );
+										Debug.Assert( tp.IsAtInsertionPosition );
+
+										RectInfo rect_info = new RectInfo { nextRect = start_rect };
+
+										bool is_left_start = true;
+
+										for(; ; )
+										{
+											Rect r = Rect.Empty;
+
+											for(; ; )
+											{
+												rect_info = GetRectInfo( tp, rect_info.nextRect );
+												r.Union( rect_info.thisRect );
+
+												if( rect_info.nextPointer == null || !IsBefore( rect_info.nextPointer, adjusted_end ) ) { tp = null; break; }
+												tp = rect_info.nextPointer;
+												if( !rect_info.nextIsSameLine ) { break; }
+											}
+
+											DrawUnderline( dc, r, is_left_start, tp == null );
+
+											if( tp == null ) break;
+
+											is_left_start = false;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			dc.Pop( );
+		}
+
+
+		void DrawUnderline( DrawingContext dc, Rect rect, bool isLeftStart, bool isRightEnd )
+		{
+			var guidelines = new GuidelineSet( );
+
+			guidelines.GuidelinesX.Add( Math.Floor( rect.Left ) );
+			guidelines.GuidelinesX.Add( Math.Ceiling( rect.Right ) );
+			guidelines.GuidelinesY.Add( Math.Ceiling( rect.Bottom ) );
+
+			guidelines.Freeze( );
+
+			dc.PushGuidelineSet( guidelines );
+
+			/*
               
             Too academic and does not look great: 
 
@@ -174,101 +174,101 @@ namespace RegExpressWPF.Adorners
 
             */
 
-            // "Worse is better":
+			// "Worse is better":
 
-            var bottom_left = rect.BottomLeft;
-            var bottom_right = rect.BottomRight;
+			var bottom_left = rect.BottomLeft;
+			var bottom_right = rect.BottomRight;
 
-            dc.DrawLine( Pen, bottom_left, bottom_right );
+			dc.DrawLine( Pen, bottom_left, bottom_right );
 
-            if( isLeftStart )
-            {
-                dc.DrawLine( Pen, bottom_left, bottom_left + new Vector( 0, -3 ) );
-            }
+			if( isLeftStart )
+			{
+				dc.DrawLine( Pen, bottom_left, bottom_left + new Vector( 0, -3 ) );
+			}
 
-            if( isRightEnd )
-            {
-                dc.DrawLine( Pen, bottom_right, bottom_right + new Vector( 0, -3 ) );
-            }
+			if( isRightEnd )
+			{
+				dc.DrawLine( Pen, bottom_right, bottom_right + new Vector( 0, -3 ) );
+			}
 
-            dc.Pop( );
-        }
-
-
-        struct RectInfo
-        {
-            public Rect thisRect;
-            public TextPointer nextPointer;
-            public Rect nextRect;
-            public bool nextIsSameLine;
-        }
+			dc.Pop( );
+		}
 
 
-        RectInfo GetRectInfo( TextPointer thisPointer, Rect thisLeadingRect )
-        {
-            Debug.Assert( thisPointer.IsAtInsertionPosition );
-            var nextPointer = thisPointer.GetNextInsertionPosition( LogicalDirection.Forward );
-
-            if( nextPointer == null )
-            {
-                return new RectInfo
-                {
-                    thisRect = new Rect( thisLeadingRect.TopLeft, new Size( 0, thisLeadingRect.Height ) ),
-                    nextPointer = null,
-                    nextRect = Rect.Empty,
-                    nextIsSameLine = false
-                };
-            }
-
-            var next_rect = nextPointer.GetCharacterRect( LogicalDirection.Forward );
-
-            if( next_rect.Top < thisLeadingRect.Bottom )
-            {
-                return new RectInfo
-                {
-                    thisRect = new Rect( thisLeadingRect.TopLeft, next_rect.BottomLeft ),
-                    nextPointer = nextPointer,
-                    nextRect = next_rect,
-                    nextIsSameLine = true
-                };
-            }
-            else
-            {
-                char[] c = new char[1];
-                int n = thisPointer.GetTextInRun( LogicalDirection.Forward, c, 0, 1 );
-
-                return new RectInfo
-                {
-                    // TODO: avoid hardcoded width
-                    thisRect = new Rect( thisLeadingRect.TopLeft, new Size( n == 0 ? 0 : 10, thisLeadingRect.Height ) ),
-                    nextPointer = nextPointer,
-                    nextRect = next_rect,
-                    nextIsSameLine = false
-                };
-            }
-        }
+		struct RectInfo
+		{
+			public Rect thisRect;
+			public TextPointer nextPointer;
+			public Rect nextRect;
+			public bool nextIsSameLine;
+		}
 
 
-        static bool IsBefore( TextPointer tp1, TextPointer tp2 )
-        {
-            return tp1.CompareTo( tp2 ) < 0;
-        }
+		RectInfo GetRectInfo( TextPointer thisPointer, Rect thisLeadingRect )
+		{
+			Debug.Assert( thisPointer.IsAtInsertionPosition );
+			var nextPointer = thisPointer.GetNextInsertionPosition( LogicalDirection.Forward );
+
+			if( nextPointer == null )
+			{
+				return new RectInfo
+				{
+					thisRect = new Rect( thisLeadingRect.TopLeft, new Size( 0, thisLeadingRect.Height ) ),
+					nextPointer = null,
+					nextRect = Rect.Empty,
+					nextIsSameLine = false
+				};
+			}
+
+			var next_rect = nextPointer.GetCharacterRect( LogicalDirection.Forward );
+
+			if( next_rect.Top < thisLeadingRect.Bottom )
+			{
+				return new RectInfo
+				{
+					thisRect = new Rect( thisLeadingRect.TopLeft, next_rect.BottomLeft ),
+					nextPointer = nextPointer,
+					nextRect = next_rect,
+					nextIsSameLine = true
+				};
+			}
+			else
+			{
+				char[] c = new char[1];
+				int n = thisPointer.GetTextInRun( LogicalDirection.Forward, c, 0, 1 );
+
+				return new RectInfo
+				{
+					// TODO: avoid hardcoded width
+					thisRect = new Rect( thisLeadingRect.TopLeft, new Size( n == 0 ? 0 : 10, thisLeadingRect.Height ) ),
+					nextPointer = nextPointer,
+					nextRect = next_rect,
+					nextIsSameLine = false
+				};
+			}
+		}
 
 
-        void Invalidate( )
-        {
-            Dispatcher.BeginInvoke( DispatcherPriority.Background, new Action( InvalidateVisual ) );
-        }
+		static bool IsBefore( TextPointer tp1, TextPointer tp2 )
+		{
+			return tp1.CompareTo( tp2 ) < 0;
+		}
 
 
-        internal void SetRangesToUnderline( IReadOnlyList<(TextPointer start, TextPointer end)> ranges )
-        {
-            lock( this )
-            {
-                Ranges = ranges;
-            }
+		void Invalidate( )
+		{
+			Dispatcher.BeginInvoke( DispatcherPriority.Background, new Action( InvalidateVisual ) );
+		}
 
-            Invalidate( );
-        }
-    }
+
+		internal void SetRangesToUnderline( IReadOnlyList<(TextPointer start, TextPointer end)> ranges )
+		{
+			lock( this )
+			{
+				Ranges = ranges;
+			}
+
+			Invalidate( );
+		}
+	}
 }
--- RegExpressWPF/Adorners/WhitespaceAdorner.cs
+++ RegExpressWPF/Adorners/WhitespaceAdorner.cs
@@ -16,188 +16,249 @@ using System.Windows.Threading;
 
 namespace RegExpressWPF.Adorners
 {
-    class WhitespaceAdorner : Adorner
-    {
-        readonly Pen WsPen = new Pen( Brushes.Transparent, 0 );
-        readonly Brush WsBrush = Brushes.LightSeaGreen;
-        readonly Pen TabPen = new Pen( Brushes.LightSeaGreen, 1 );
-        readonly Pen EofPen = new Pen( Brushes.LightSeaGreen, 1 );
-        readonly Brush EofBrush = Brushes.Transparent;
+	class WhitespaceAdorner : Adorner
+	{
+		readonly Pen WsPen = new Pen( Brushes.Transparent, 0 );
+		readonly Brush WsBrush = Brushes.LightSeaGreen;
+		readonly Pen TabPen = new Pen( Brushes.LightSeaGreen, 1 );
+		readonly Pen EofPen = new Pen( Brushes.LightSeaGreen, 1 );
+		readonly Brush EofBrush = Brushes.Transparent;
 
 
-        public WhitespaceAdorner( UIElement adornedElement ) : base( adornedElement )
-        {
-            Debug.Assert( adornedElement is MyRichTextBox );
+		public WhitespaceAdorner( UIElement adornedElement ) : base( adornedElement )
+		{
+			Debug.Assert( adornedElement is MyRichTextBox );
 
-            IsHitTestVisible = false;
+			IsHitTestVisible = false;
 
-            Rtb.TextChanged += Rtb_TextChanged;
-            Rtb.AddHandler( ScrollViewer.ScrollChangedEvent, new RoutedEventHandler( Rtb_ScrollChanged ), true );
-        }
-
-
-        MyRichTextBox Rtb
-        {
-            get { return (MyRichTextBox)AdornedElement; }
-        }
+			Rtb.TextChanged += Rtb_TextChanged;
+			Rtb.AddHandler( ScrollViewer.ScrollChangedEvent, new RoutedEventHandler( Rtb_ScrollChanged ), true );
+		}
 
 
-        private void Rtb_TextChanged( object sender, TextChangedEventArgs e )
-        {
-            Invalidate( );
-        }
+		MyRichTextBox Rtb
+		{
+			get { return (MyRichTextBox)AdornedElement; }
+		}
 
 
-        private void Rtb_ScrollChanged( object sender, RoutedEventArgs e )
-        {
-            Invalidate( );
-        }
+		private void Rtb_TextChanged( object sender, TextChangedEventArgs e )
+		{
+			Invalidate( );
+		}
 
 
-        protected override void OnRender( DrawingContext drawingContext )
-        {
-            base.OnRender( drawingContext );  // (probably nothing)
-
-            var dc = drawingContext;
-            var rtb = Rtb;
-            var td = rtb.GetTextData( null );
-
-            ShowSpaces( dc, td );
-            ShowTabs( dc, td );
-            ShowEof( dc, td );
-        }
+		private void Rtb_ScrollChanged( object sender, RoutedEventArgs e )
+		{
+			Invalidate( );
+		}
 
 
-        protected override void OnRenderSizeChanged( SizeChangedInfo sizeInfo )
-        {
-            base.OnRenderSizeChanged( sizeInfo );
+		protected override void OnRender( DrawingContext drawingContext )
+		{
+			base.OnRender( drawingContext );  // (probably nothing)
 
-            Invalidate( );
-        }
+			var dc = drawingContext;
+			var rtb = Rtb;
+			var td = rtb.GetTextData( null );
 
-
-        void Invalidate( )
-        {
-            Dispatcher.BeginInvoke( DispatcherPriority.Background, new Action( InvalidateVisual ) );
-        }
-
-
-        void ShowSpaces( DrawingContext dc, TextData td )
-        {
-            var rtb = Rtb;
-
-            var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
-            dc.PushClip( new RectangleGeometry( clip_rect ) );
-
-            foreach( Match m in Regex.Matches( td.Text, @"(\p{Zs}+)(?:\n|\r|$)" ) )
-            {
-                var g = m.Groups[1];
-                var start = td.Pointers[g.Index];
-                var end = td.Pointers[g.Index + g.Length];
-                if( start.HasValidLayout && end.HasValidLayout )
-                {
-                    var start_rect = start.GetCharacterRect( LogicalDirection.Forward );
-                    var end_rect = end.GetCharacterRect( LogicalDirection.Backward );
-
-                    // TODO: consider wrapped text
-
-                    var w = end_rect.Right - start_rect.Left;
-
-                    if( w > 0 )
-                    {
-                        var r = new Rect( start_rect.Left, start_rect.Top, w, start_rect.Height );
-
-                        if( !r.IsEmpty && r.IntersectsWith( clip_rect ) )
-                        {
-                            var y = r.Top + r.Height / 2;
-
-                            for( var i = 0; i < g.Length; ++i )
-                            {
-                                var x = r.Left + r.Width * i / g.Length + r.Width / g.Length / 2;
-
-                                var p = new Rect( x, y, 2, 2 );
-
-                                dc.DrawRectangle( WsBrush, WsPen, p );
-                            }
-                        }
-                    }
-                }
-            }
-
-            dc.Pop( );
-        }
+			ShowSpaces( dc, td );
+			ShowTabs( dc, td );
+			//ShowEof( dc, td );
+		}
 
 
-        void ShowTabs( DrawingContext dc, TextData td )
-        {
-            var rtb = Rtb;
+		protected override void OnRenderSizeChanged( SizeChangedInfo sizeInfo )
+		{
+			base.OnRenderSizeChanged( sizeInfo );
 
-            var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
-            dc.PushClip( new RectangleGeometry( clip_rect ) );
-
-            var half_pen_th = TabPen.Thickness / 2;
-
-            foreach( Match m in Regex.Matches( td.Text, @"\t" ) )
-            {
-                var start = td.Pointers[m.Index];
-
-                if( start.HasValidLayout )
-                {
-                    var r = start.GetCharacterRect( LogicalDirection.Forward );
-                    const int w = 8;
-                    r.Width = w;
-                    r.Offset( 2, 0 );
-                    if( r.IntersectsWith( clip_rect ) )
-                    {
-                        var x = r.Left;
-                        var y = r.Top + r.Height / 2.0;
-
-                        var guidelines = new GuidelineSet( );
-                        guidelines.GuidelinesY.Add( y - half_pen_th );
-
-                        dc.PushGuidelineSet( guidelines );
-
-                        dc.DrawLine( TabPen, new Point( x, y ), new Point( x + w, y ) );
-                        dc.DrawLine( TabPen, new Point( x + w / 2, y - w / 2 ), new Point( x + w, y ) );
-                        dc.DrawLine( TabPen, new Point( x + w / 2, y + w / 2 ), new Point( x + w, y ) );
-
-                        dc.Pop( );
-                    }
-                }
-            }
-
-            dc.Pop( );
-        }
+			Invalidate( );
+		}
 
 
-        void ShowEof( DrawingContext dc, TextData td )
-        {
-            if( Regex.IsMatch( td.Text, @"(\r|\n)$", RegexOptions.ExplicitCapture ) )
-            {
-                var rtb = Rtb;
+		void Invalidate( )
+		{
+			Dispatcher.BeginInvoke( DispatcherPriority.Background, new Action( InvalidateVisual ) );
+		}
 
-                if( rtb.Document.ContentEnd.HasValidLayout )
-                {
-                    var rect = rtb.Document.ContentEnd.GetCharacterRect( LogicalDirection.Forward ); // (no width)
 
-                    if( !rect.IsEmpty )
-                    {
-                        double w = 5;
-                        double h = rect.Height * 0.4;
-                        double top = rect.Top + ( rect.Height - h ) / 2.0;
+		void ShowSpaces( DrawingContext dc, TextData td )
+		{
+			/*
+			var rtb = Rtb;
 
-                        var eof_rect = new Rect( new Point( rect.Left + 3, top ), new Size( w, h ) );
-                        var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+			dc.PushClip( new RectangleGeometry( clip_rect ) );
 
-                        dc.PushClip( new RectangleGeometry( clip_rect ) );
+			foreach( Match m in Regex.Matches( td.Text, @"(\p{Zs}+)(?:\n|\r|$)" ) )
+			{
+				var g = m.Groups[1];
+				var start = td.Pointers[g.Index];
+				var end = td.Pointers[g.Index + g.Length];
+				if( start.HasValidLayout && end.HasValidLayout )
+				{
+					var start_rect = start.GetCharacterRect( LogicalDirection.Forward );
+					var end_rect = end.GetCharacterRect( LogicalDirection.Backward );
 
-                        dc.DrawRectangle( EofBrush, EofPen, eof_rect );
+					// TODO: consider wrapped text
 
-                        dc.Pop( );
-                    }
-                }
-            }
-        }
-    }
+					var w = end_rect.Right - start_rect.Left;
+
+					if( w > 0 )
+					{
+						var r = new Rect( start_rect.Left, start_rect.Top, w, start_rect.Height );
+
+						if( !r.IsEmpty && r.IntersectsWith( clip_rect ) )
+						{
+							var y = r.Top + r.Height / 2;
+
+							for( var i = 0; i < g.Length; ++i )
+							{
+								var x = r.Left + r.Width * i / g.Length + r.Width / g.Length / 2;
+
+								var p = new Rect( x, y, 2, 2 );
+
+								dc.DrawRectangle( WsBrush, WsPen, p );
+							}
+						}
+					}
+				}
+			}
+
+			dc.Pop( );
+			*/
+
+			const string pattern = @"(\p{Zs})\1+";
+			var re = new Regex( pattern );
+
+			var rtb = Rtb;
+			var start_doc = Rtb.Document.ContentStart;
+
+			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+			dc.PushClip( new RectangleGeometry( clip_rect ) );
+
+			var guidelines = new GuidelineSet( );
+
+
+			foreach( Match match in re.Matches( td.Text ) )
+			{
+				var left = td.Pointers[match.Index];
+				var right = td.Pointers[match.Index + match.Length];
+
+				if( left.HasValidLayout && right.HasValidLayout &&
+					left.IsInSameDocument( start_doc ) && right.IsInSameDocument( start_doc ) )
+				{
+					left = left.GetInsertionPosition( LogicalDirection.Forward );
+					right = right.GetInsertionPosition( LogicalDirection.Backward );
+
+					var rect_left = left.GetCharacterRect( LogicalDirection.Forward );
+					var rect_right = right.GetCharacterRect( LogicalDirection.Backward );
+
+					var rect = new Rect( rect_left.TopLeft, rect_right.BottomRight );
+
+					if( !rect.IsEmpty && rect.IntersectsWith( clip_rect ) )
+					{
+						const int DOT_SIZE = 2;
+						var y = Math.Ceiling( rect.Top + rect.Height / 2 - DOT_SIZE / 2);
+						var w = rect.Width / match.Length;
+
+						guidelines.GuidelinesY.Clear( );
+						guidelines.GuidelinesY.Add( y );
+
+						dc.PushGuidelineSet( guidelines );
+
+
+						for( var i = 0; i < match.Length; ++i )
+						{
+							var x = rect.Left + rect.Width * i / match.Length + w / 2;
+							var p = new Rect( x, y, DOT_SIZE, DOT_SIZE );
+
+							dc.DrawRectangle( WsBrush, WsPen, p );
+						}
+
+						dc.Pop( );
+					}
+				}
+			}
+
+			dc.Pop( );
+		}
+
+
+		void ShowTabs( DrawingContext dc, TextData td )
+		{
+			var rtb = Rtb;
+			var start_doc = Rtb.Document.ContentStart;
+
+			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+			dc.PushClip( new RectangleGeometry( clip_rect ) );
+
+			var guidelines = new GuidelineSet( );
+
+			foreach( Match match in Regex.Matches( td.Text, @"\t" ) )
+			{
+				var left = td.Pointers[match.Index];
+
+				if( left.HasValidLayout && left.IsInSameDocument( start_doc ) )
+				{
+					left = left.GetInsertionPosition( LogicalDirection.Forward );
+
+					var rect = left.GetCharacterRect( LogicalDirection.Forward );
+					const int ARROW_WIDTH = 8;
+					rect.Width = ARROW_WIDTH;
+					rect.Offset( 2, 0 );
+
+					if( rect.IntersectsWith( clip_rect ) )
+					{
+						var x = rect.Left;
+						var y = Math.Ceiling( rect.Top + rect.Height / 2 );
+
+						guidelines.GuidelinesY.Clear( );
+						guidelines.GuidelinesY.Add( y );
+
+						dc.PushGuidelineSet( guidelines );
+
+						dc.DrawLine( TabPen, new Point( x, y ), new Point( x + ARROW_WIDTH, y ) );
+						dc.DrawLine( TabPen, new Point( x + ARROW_WIDTH / 2, y - ARROW_WIDTH / 2 ), new Point( x + ARROW_WIDTH, y ) );
+						dc.DrawLine( TabPen, new Point( x + ARROW_WIDTH / 2, y + ARROW_WIDTH / 2 ), new Point( x + ARROW_WIDTH, y ) );
+
+						dc.Pop( );
+					}
+				}
+			}
+
+			dc.Pop( );
+		}
+
+
+		void ShowEof( DrawingContext dc, TextData td )
+		{
+			if( Regex.IsMatch( td.Text, @"(\r|\n)$", RegexOptions.ExplicitCapture ) )
+			{
+				var rtb = Rtb;
+
+				if( rtb.Document.ContentEnd.HasValidLayout )
+				{
+					var rect = rtb.Document.ContentEnd.GetCharacterRect( LogicalDirection.Forward ); // (no width)
+
+					if( !rect.IsEmpty )
+					{
+						double w = 5;
+						double h = rect.Height * 0.4;
+						double top = rect.Top + ( rect.Height - h ) / 2.0;
+
+						var eof_rect = new Rect( new Point( rect.Left + 3, top ), new Size( w, h ) );
+						var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+
+						dc.PushClip( new RectangleGeometry( clip_rect ) );
+
+						dc.DrawRectangle( EofBrush, EofPen, eof_rect );
+
+						dc.Pop( );
+					}
+				}
+			}
+		}
+	}
 }
 
