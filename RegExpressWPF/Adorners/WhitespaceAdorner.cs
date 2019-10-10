5016a1c1a1c8099ea4a16c3d639eaece0bacec5a
Viorel <Viorel@noreply.github.com>
2019-10-10 20:27:23 +0300

    Fixed: Defective whitespace marks in bidirectional texts.

--- RegExpressWPF/Adorners/WhitespaceAdorner.cs
+++ RegExpressWPF/Adorners/WhitespaceAdorner.cs
@@ -28,6 +28,8 @@ namespace RegExpressWPF.Adorners
 
 		bool mShowWhitespaces = false;
 
+		public bool IsDbgDisabled; // (disable this adorner for debugging purposes)
+
 
 		public WhitespaceAdorner( UIElement adornedElement ) : base( adornedElement )
 		{
@@ -76,6 +78,8 @@ namespace RegExpressWPF.Adorners
 		{
 			base.OnRender( drawingContext );  // (probably nothing)
 
+			if( IsDbgDisabled ) return;
+
 			if( mShowWhitespaces )
 			{
 				var dc = drawingContext;
@@ -114,26 +118,20 @@ namespace RegExpressWPF.Adorners
 			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
 			dc.PushClip( new RectangleGeometry( clip_rect ) );
 
-			TextPointer start_pointer = rtb.GetPositionFromPoint( new Point( 0, 0 ), true );
-			TextPointer end_pointer = rtb.GetPositionFromPoint( new Point( rtb.ViewportWidth, rtb.ViewportHeight ), true );
-
-			start_pointer = start_pointer.GetInsertionPosition( LogicalDirection.Forward );
-			end_pointer = end_pointer.GetInsertionPosition( LogicalDirection.Backward );
-
+			TextPointer start_pointer = rtb.GetPositionFromPoint( new Point( 0, 0 ), true ).GetLineStartPosition( -1, out int unused );
 			int start_i = RtbUtilities.FindNearestBefore( td.Pointers, start_pointer );
-			int end_i = RtbUtilities.FindNearestAfter( td.Pointers, end_pointer );
 
 			if( start_i < 0 ) start_i = 0;
-			Debug.Assert( end_i >= 0 );
-			if( end_i < 0 ) end_i = td.Pointers.Count - 1;
 
-			for( var i = td.Text.IndexOfAny( Characters, start_i ); i >= 0 && i <= end_i; i = td.Text.IndexOfAny( Characters, i + 1 ) )
+			for( var i = td.Text.IndexOfAny( Characters, start_i ); i >= 0; i = td.Text.IndexOfAny( Characters, i + 1 ) )
 			{
 				var left = td.Pointers[i];
 				var right = td.Pointers[i + 1];
 
 				var rect_left = left.GetCharacterRect( LogicalDirection.Forward );
 				var rect_right = right.GetCharacterRect( LogicalDirection.Backward );
+
+				if( rect_left.Top > clip_rect.Bottom ) break;
 
 				switch( td.Text[i] )
 				{
@@ -158,20 +156,7 @@ namespace RegExpressWPF.Adorners
 					case '\r':
 					case '\n':
 					{
-						const int EOL_WIDTH = 6;
-
-						var rect = rect_left;
-						rect.Offset( 2, 0 );
-
-						var half_pen = EolPen.Thickness / 2;
-
-						var x = Math.Ceiling( rect.Left ) + half_pen;
-						var y = Math.Ceiling( rect.Top + rect.Height / 2 ) - half_pen;
-
-						dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH, y ) );
-						dc.DrawLine( EolPen, new Point( x + EOL_WIDTH, y ), new Point( x + EOL_WIDTH, y - rect.Height * 0.35 ) );
-						dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH / 2, y - EOL_WIDTH / 2 ) );
-						dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH / 2, y + EOL_WIDTH / 2 ) );
+						DrawEol( dc, left, rect_left );
 					}
 					break;
 
@@ -195,25 +180,71 @@ namespace RegExpressWPF.Adorners
 		}
 
 
+		void DrawEol( DrawingContext dc, TextPointer eol_pointer, Rect eol_rect )
+		{
+			double max_x = eol_rect.Left;
+
+			for( var tp = eol_pointer; ; )
+			{
+				tp = tp.GetNextInsertionPosition( LogicalDirection.Backward );
+				if( tp == null ) break;
+
+				var rect = tp.GetCharacterRect( LogicalDirection.Forward );
+				if( rect.Bottom < eol_rect.Bottom ) break;
+				if( rect.Left < max_x ) break;
+
+				max_x = rect.Left;
+			}
+
+			const int EOL_WIDTH = 6;
+
+			var half_pen = EolPen.Thickness / 2;
+
+			var x = Math.Ceiling( max_x + 2 ) + half_pen;
+			var y = Math.Ceiling( eol_rect.Top + eol_rect.Height / 2 ) - half_pen;
+
+			dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH, y ) );
+			dc.DrawLine( EolPen, new Point( x + EOL_WIDTH, y ), new Point( x + EOL_WIDTH, y - eol_rect.Height * 0.35 ) );
+			dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH / 2, y - EOL_WIDTH / 2 ) );
+			dc.DrawLine( EolPen, new Point( x, y ), new Point( x + EOL_WIDTH / 2, y + EOL_WIDTH / 2 ) );
+		}
+
+
 		void ShowEof( DrawingContext dc )
 		{
 			var rtb = Rtb;
 
 			if( !rtb.Document.ContentEnd.HasValidLayout ) return;
 
-			var rect = rtb.Document.ContentEnd.GetCharacterRect( LogicalDirection.Forward ); // (no width)
-			if( rect.IsEmpty ) return;
-
 			var clip_rect = new Rect( new Size( rtb.ViewportWidth, rtb.ViewportHeight ) );
+
+			var end = rtb.Document.ContentEnd;
+			var end_rect = end.GetCharacterRect( LogicalDirection.Forward ); // (no width)
+
+			if( end_rect.Bottom < clip_rect.Top || end_rect.Top > clip_rect.Bottom ) return;
+
+			double max_x = end_rect.Left;
+
+			for( var tp = end; ; )
+			{
+				tp = tp.GetNextInsertionPosition( LogicalDirection.Backward );
+				if( tp == null ) break;
+
+				var rect = tp.GetCharacterRect( LogicalDirection.Forward );
+				if( rect.Bottom < end_rect.Bottom ) break;
+				if( rect.Left < max_x ) break;
+
+				max_x = rect.Left;
+			}
 
 			dc.PushClip( new RectangleGeometry( clip_rect ) );
 
 			const double EOF_WIDTH = 4;
-			double h = Math.Ceiling( rect.Height * 0.3 );
+			double h = Math.Ceiling( end_rect.Height * 0.3 );
 			double half_pen = EofPen.Thickness / 2;
 
-			var x = Math.Ceiling( rect.Left + 3 ) + half_pen;
-			var y = Math.Floor( rect.Top + ( rect.Height - h ) / 2 ) - half_pen;
+			var x = Math.Ceiling( max_x + 3 ) + half_pen;
+			var y = Math.Floor( end_rect.Top + ( end_rect.Height - h ) / 2 ) - half_pen;
 
 			var eof_rect = new Rect( x, y, EOF_WIDTH, h );
 
