7dee9559907681a455315caaf8d2f1bb098400ea
Viorel <Viorel@noreply.github.com>
2019-11-06 14:46:57 +0200

    Starting an experimental support for regular expressions from STL (Standard Template Library for C++)..

---
+++ AlternativeRegexEngines/CppRegex/CppRegex.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+
+#include "CppRegex.h"
+
+
+using namespace std;
+
+
+namespace CppRegex
+{
+	void CppRegex::Matches( String^ text0, String^ pattern0, CppRegexOptions options0 )
+	{
+		msclr::interop::marshal_context context;
+
+		wstring text = context.marshal_as<wstring>( text0 );
+		wstring pattern = context.marshal_as<wstring>( pattern0 );
+
+		wregex::flag_type flags = static_cast<wregex::flag_type>( options0 );
+
+		wregex re( pattern, flags );
+
+		auto results_begin = wsregex_iterator( text.cbegin( ), text.cend( ), re );
+		auto results_end = std::wsregex_iterator( );
+
+		for( auto i = results_begin; i != results_end; ++i )
+		{
+			const wsmatch& match = *i;
+
+			auto submatches_begin = match.cbegin( );
+			auto submatches_end = match.cend( );
+
+			for( auto j = submatches_begin; j != submatches_end; ++j )
+			{
+				wssub_match submatch = *j;
+
+				//...
+
+			}
+
+		}
+
+
+	}
+}
---
+++ AlternativeRegexEngines/CppRegex/CppRegex.h
@@ -0,0 +1,30 @@
+#pragma once
+
+using namespace System;
+
+namespace CppRegex
+{
+	enum class CppRegexOptions
+	{
+		ECMAScript = std::wregex::flag_type::ECMAScript,
+		basic = std::wregex::flag_type::basic,
+		extended = std::wregex::flag_type::extended,
+		awk = std::wregex::flag_type::awk,
+		grep = std::wregex::flag_type::grep,
+		egrep = std::wregex::flag_type::egrep,
+		//_Gmask = std::wregex::flag_type::_Gmask,
+
+		icase = std::wregex::flag_type::icase,
+		nosubs = std::wregex::flag_type::nosubs,
+		optimize = std::wregex::flag_type::optimize,
+		collate = std::wregex::flag_type::collate
+	};
+
+
+	public ref class CppRegex
+	{
+	public:
+
+		static void Matches( String^ text0, String^ pattern0, CppRegexOptions options0 );
+	};
+}
