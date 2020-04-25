bbb8f8e491dc85119329b8f4e7dc4a8da054d99e
Viorel <Viorel@noreply.github.com>
2020-04-25 23:06:23 +0300

    Fixing 'xcopy' commands.

--- RegExpressWPF/PostBuildStep.cmd
+++ RegExpressWPF/PostBuildStep.cmd
@@ -17,12 +17,12 @@ rem -- ICU --
 
 set Prj=%MainProjectPath%\..\RegexEngines\Icu\IcuRegexInterop
 
-xcopy /d "%Prj%\ICU-min\bin64\*.dll" "%TargetDir%\ICU-min\bin64\*"
+xcopy /D /R /Y "%Prj%\ICU-min\bin64\*.dll" "%TargetDir%\ICU-min\bin64\*"
 
 
 rem -- Perl 5 --
 
 set Prj=%MainProjectPath%\..\RegexEngines\Perl5\Perl5RegexEngine
 
-xcopy /E /D "%Prj%\Perl5-min\*" "%TargetDir%\Perl5-min\*"
+xcopy /E /D /R /Y "%Prj%\Perl5-min\*" "%TargetDir%\Perl5-min\*"
  
