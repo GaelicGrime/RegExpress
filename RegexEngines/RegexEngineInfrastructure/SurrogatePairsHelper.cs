767e381fd606725930343650c20252eb155cbdb0
Viorel <Viorel@noreply.github.com>
2020-05-09 14:22:36 +0300

    Fixing issues with Surrogate Pair characters, supported distinctly by Perl and Python.

--- RegexEngines/RegexEngineInfrastructure/SurrogatePairsHelper.cs
+++ RegexEngines/RegexEngineInfrastructure/SurrogatePairsHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,40 @@ namespace RegexEngineInfrastructure
 {
 	public sealed class SurrogatePairsHelper
 	{
+		readonly List<int> SurrogatePairs = new List<int>( );
+
 		public SurrogatePairsHelper( string text )
 		{
 			CollectSurrogatePairs( text );
 		}
 
 
-		void CollectSurrogatePairs( string text )
+		public int GetAlternativeIndex(int index)
 		{
 			throw new NotImplementedException( );
+		}
+
+
+		void CollectSurrogatePairs( string text )
+		{
+			SurrogatePairs.Clear( );
+
+			for( int i = 0; i < text.Length; )
+			{
+				if( char.IsSurrogatePair( text, i ) )
+				{
+					Debug.Assert( i <= text.Length - 2 );
+					if( i <= text.Length - 2 )
+					{
+						SurrogatePairs.Add( i );
+					}
+					i += 2;
+				}
+				else
+				{
+					++i;
+				}
+			}
 		}
 	}
 }
