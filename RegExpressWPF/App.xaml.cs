3b9380760799e4ef80aca320016393c1334190e6
Viorel <Viorel@noreply.github.com>
2021-04-28 12:09:59 +0300

    Single instance mode (attempt 1).

--- RegExpressWPF/App.xaml.cs
+++ RegExpressWPF/App.xaml.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 
 namespace RegExpressWPF
@@ -15,6 +19,20 @@ namespace RegExpressWPF
 	/// </summary>
 	public partial class App : Application
 	{
+		[DllImport( "user32" )]
+		static extern bool AllowSetForegroundWindow( Int32 pid );
+
+		[DllImport( "user32" )]
+		static extern bool SetForegroundWindow( IntPtr hWnd );
+
+
+		const string SingleInstanceMutexName = "RegExpress-SingleInstance-Mutex-1";
+		const string SingleInstanceEventName = "RegExpress-SingleInstance-Event-1";
+
+		Mutex mSingleInstanceMutex = null;
+		EventWaitHandle mSingleInstanceEvent;
+
+
 		public App( )
 		{
 			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
@@ -22,6 +40,74 @@ namespace RegExpressWPF
 			//DispatcherUnhandledException += App_DispatcherUnhandledException;
 			//TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 		}
+
+
+		private void App_Startup( object sender, StartupEventArgs e )
+		{
+			Debug.Assert( mSingleInstanceMutex == null );
+
+			bool new_mutex_created;
+			mSingleInstanceMutex = new Mutex( false, SingleInstanceMutexName, out new_mutex_created );
+
+			if( new_mutex_created )
+			{
+				bool new_event_created;
+				mSingleInstanceEvent = new EventWaitHandle( false, EventResetMode.AutoReset, SingleInstanceEventName, out new_event_created );
+
+				Debug.Assert( new_event_created );
+
+				var thread = new Thread( SingleInstanceThreadProc ) { IsBackground = true };
+				thread.Start( );
+			}
+			else
+			{
+				var current_process = Process.GetCurrentProcess( );
+				var other_process = Process.GetProcessesByName( current_process.ProcessName ).FirstOrDefault( p => p.Id != current_process.Id );
+				Debug.Assert( other_process != null );
+
+				SetForegroundWindow( other_process.MainWindowHandle );
+
+				if( other_process != null )
+				{
+					AllowSetForegroundWindow( other_process.Id );
+				}
+
+				mSingleInstanceEvent = new EventWaitHandle( false, EventResetMode.AutoReset, SingleInstanceEventName );
+				mSingleInstanceEvent.Set( );
+
+				Shutdown( );
+			}
+		}
+
+
+		private void SingleInstanceThreadProc( )
+		{
+			for(; ; )
+			{
+				mSingleInstanceEvent.WaitOne( );
+
+				Dispatcher.BeginInvoke( DispatcherPriority.Normal,
+					new Action( ( ) =>
+					{
+						try
+						{
+							var current_process = Process.GetCurrentProcess( );
+							var current_main_hwnd = current_process.MainWindowHandle;
+
+							SetForegroundWindow( current_main_hwnd );
+							//MainWindow.Focus( );
+						}
+						catch( Exception exc )
+						{
+							_ = exc;
+							if( Debugger.IsAttached ) Debugger.Break( );
+
+							// ignore
+						}
+					} ) );
+			}
+		}
+
 
 		//private void TaskScheduler_UnobservedTaskException( object sender, UnobservedTaskExceptionEventArgs e )
 		//{
